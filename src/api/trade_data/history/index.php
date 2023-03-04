6e2080966f7c7f70023d084fe554e33153e42707
James <jmm51@st-andrews.ac.uk>
2023-03-04 16:31:26 +0000

    dashboard histroy chart

---
+++ src/api/trade_data/history/index.php
@@ -0,0 +1,42 @@
+<?php
+
+include '../global/connection.php';
+
+$chartTime = "all";
+if(isset($_POST['chartTime'])){
+    $chartTime = $_POST['chartTime'];
+}
+
+$query_condition = "";
+$now = date("Y-m-d H:i:s");
+
+if($chartTime == "week"){
+    $query_condition = "WHERE time BETWEEN '".date("Y-m-d H:i:s", strtotime("-1 week"))."' AND '".$now."'";
+} else if($chartTime == "month"){
+    $query_condition = "WHERE time BETWEEN '".date("Y-m-d H:i:s", strtotime("-1 month"))."' AND '".$now."'";
+} else if($chartTime == "year"){
+    $query_condition = "WHERE time BETWEEN '".date("Y-m-d H:i:s", strtotime("-1 year"))."' AND '".$now."'";
+}
+
+// Prepare and execute query to fetch all trades
+$sql = "SELECT * FROM history " . $query_condition;
+$result = mysqli_query($conn, $sql);
+
+// Create an array to hold the trades
+$history = array();
+
+// Loop through each trade and add it to the array
+while ($row = mysqli_fetch_array($result)) {
+    array_push($history, $row);
+}
+
+// Convert the trades array to a JSON string
+$json = json_encode($history);
+
+// Output the JSON string
+echo $json;
+
+// Close connection
+mysqli_close($conn);
+
+?>
