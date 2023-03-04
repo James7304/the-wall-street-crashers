0c2ad88deac25eb6d4fe993c226a0c45889de082
James <jmm51@st-andrews.ac.uk>
2023-03-04 19:23:33 +0000

    link fix

--- src/api/stripe/webhooks/deposit/index.php
+++ src/api/stripe/webhooks/deposit/index.php
@@ -20,7 +20,7 @@ $stripe = new \Stripe\StripeClient('sk_test_51MhtvNIbq7fKxkhRCIyMxtpdd3NjZkUyLTd
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 $endpoint_secret = 'whsec_ddf82a3c6c2e868d1fb3d948c1a70d366d90b39c89171d448f61cfbc3bb29c3a';
 
-$payload = @file_get_contents('php://input');
+$payload = file_get_contents('php://input');
 $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
 $event = null;
 
--- src/deposit/pay/scripts/checkout.js
+++ src/deposit/pay/scripts/checkout.js
@@ -46,7 +46,7 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url: "http://localhost:8080/deposit/success",
+      return_url: "http://localhost:8080/deposit/success/?amount=" + amount.amount,
       receipt_email: emailAddress,
     },
   });
