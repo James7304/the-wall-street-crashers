58400ab24a0ca5f804a8024d6338177cf33fd8d3
James <jmm51@st-andrews.ac.uk>
2023-03-04 20:12:29 +0000

    shares

--- src/api/stripe/webhooks/deposit/index.php
+++ src/api/stripe/webhooks/deposit/index.php
@@ -48,10 +48,10 @@ switch ($event->type) {
     $shares = mysqli_query($conn, "SELECT SUM(shares) FROM users");
     $value = mysqli_query($conn, "SELECT quantity * price_per_unit FROM portfolio");
 
-    $share_price = mysqli_fetch_row($value)[0];
+    $shares_issued = mysqli_fetch_row($value)[0];
 
     $share_price = intval($share_price)/intval(mysqli_fetch_row($shares)[0]);
-    echo $share_price;
+    echo $shares_issued;
 
     mysqli_query($conn, "UPDATE users SET deposited = deposited + ".$paymentIntent->amount_received.", shares = shares + ".$paymentIntent->amount_received/$share_price." WHERE user_acc = '".$paymentIntent->metadata->user_acc."'");
     mysqli_query($conn, "UPDATE portfolio SET quantity = quantity + ".$paymentIntent->amount_received." WHERE ticker = 'cash'");
