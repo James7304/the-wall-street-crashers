636f2cafbc8c2771b4a7e56e66bbbc8099383174
James <jmm51@st-andrews.ac.uk>
2023-03-04 19:12:44 +0000

    stripe webhook test 1

--- src/api/stripe/webhooks/deposit/index.php
+++ src/api/stripe/webhooks/deposit/index.php
@@ -15,7 +15,7 @@ require 'vendor/autoload.php';
 
 // The library needs to be configured with your account's secret key.
 // Ensure the key is kept out of any version control system you might be using.
-$stripe = new \Stripe\StripeClient('sk_test_...');
+$stripe = new \Stripe\StripeClient('sk_test_51MhtvNIbq7fKxkhRCIyMxtpdd3NjZkUyLTdS2ehAHjZM6sWuU5YGp5iEGTAdBMyET6mGYlrs1g7Y7W9CUzEc9lo000m6UGyp7a');
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 $endpoint_secret = 'whsec_ddf82a3c6c2e868d1fb3d948c1a70d366d90b39c89171d448f61cfbc3bb29c3a';
@@ -42,7 +42,11 @@ try {
 switch ($event->type) {
   case 'payment_intent.succeeded':
     $paymentIntent = $event->data->object;
-  // ... handle other event types
+
+    //include '../../global/connection.php';
+    //mysqli_query($conn, "INSERT INTO ");
+    echo "success";
+
   default:
     echo 'Received unknown event type ' . $event->type;
 }
