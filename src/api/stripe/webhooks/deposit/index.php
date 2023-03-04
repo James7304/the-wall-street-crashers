fdcf3057ab747ba2cf56a0f50fd0981001dd7464
James <jmm51@st-andrews.ac.uk>
2023-03-04 19:29:54 +0000

    stripe webhook test 2

--- src/api/stripe/webhooks/deposit/index.php
+++ src/api/stripe/webhooks/deposit/index.php
@@ -11,7 +11,7 @@
 // 3) Run the server on http://localhost:4242
 //   php -S localhost:4242
 
-require 'vendor/autoload.php';
+require '../../../../vendor/autoload.php';
 
 // The library needs to be configured with your account's secret key.
 // Ensure the key is kept out of any version control system you might be using.
