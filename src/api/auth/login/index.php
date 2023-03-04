ce98b3c32658eda11660b705c7f8a05ee00ce3ed
James <jmm51@st-andrews.ac.uk>
2023-03-04 12:16:08 +0000

    login backend

---
+++ src/api/auth/login/index.php
@@ -0,0 +1,42 @@
+
+<?php
+
+include '../global/connection.php';
+
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$res = array('login' => false);
+
+// Check if email and password were sent in POST request
+if(isset($_POST['email']) && isset($_POST['password'])){
+
+    // Get email and password from POST request
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Encrypt password using SHA512
+    $encrypted_password = hash('sha512', $password);
+
+    // Prepare and execute query to fetch user from database
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$encrypted_password'";
+    $result = mysqli_query($conn, $sql);
+
+    // Check if user was found
+    if (mysqli_num_rows($result) > 0) {
+        $res = array('login' => true);
+        session_start();
+        $_SESSION['user_id'] = mysqli_fetch_array($result)['user_id'];
+    }
+
+}
+
+// Close connection
+mysqli_close($conn);
+
+echo json_encode($res);
+
+?>
