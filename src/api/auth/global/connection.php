8e6b7d4b69e0203db13a3a541e18bca9c69dab02
James <jmm51@st-andrews.ac.uk>
2023-03-05 10:18:21 +0000

    conn change

--- src/api/auth/global/connection.php
+++ src/api/auth/global/connection.php
@@ -2,9 +2,9 @@
 
 // Set the database credentials
 $servername = "sql750.main-hosting.eu";
-$username = "u202629177_wsc_2";
+$username = "u202629177_wsc";
 $password = "z0|xo@!K";
-$dbname = "u202629177_wsc_2";
+$dbname = "u202629177_wsc";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
