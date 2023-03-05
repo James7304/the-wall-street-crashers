a2a0323b853dd12b39d450daa8d7710954dae314
James7304 <jamesmccorkindale0@gmail.com>
2023-03-05 05:17:25 +0000

    motion detection

---
+++ src/motion_detector/dbapi.py
@@ -0,0 +1,112 @@
+import pymysql.cursors
+
+
+# # Set the database credentials
+# host = "sql750.main-hosting.eu"
+# user = "u202629177_wsc"
+# password = "z0|xo@!K"
+# database = "u202629177_wsc"
+#
+# # Create a connection to the database
+# conn = pymysql.connect(
+#     host=host,
+#     user=user,
+#     password=password,
+#     database=database,
+#     cursorclass=pymysql.cursors.DictCursor
+# )
+#
+# # Create a cursor object to execute SQL queries
+# cursor = conn.cursor()
+
+class DBAPI:
+    def __init__(self, type):
+        # Set the database credentials
+        host = "sql750.main-hosting.eu"
+        user = "u202629177_wsc"
+        password = "z0|xo@!K"
+        database = "u202629177_wsc"
+
+        self.type = type
+
+        # Create a connection to the database
+        self.conn = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+        # Create a cursor object to execute SQL queries
+        self.cursor = self.conn.cursor()
+
+    def buy_stock(self, ticker, n, price):
+        self.insert_stock(ticker, n, price)
+        self.insert_trade(ticker, 'BUY', n, price)
+        self.valuation = self.get_valuation()
+
+    def sell_stock(self, ticker, n):
+        price = self.get_stock(ticker)["price_per_unit"]
+        self.drop_stock(ticker, n)
+        self.insert_trade(ticker, 'SELL', n, price)
+        self.valuation = self.get_valuation()
+
+    def insert_stock(self, ticker, n, price):
+        self.cursor.execute(
+            f"insert into " + self.type + "portfolio (ticker, quantity, price_per_unit) values ('{ticker}', {n}, {price})")
+        self.conn.commit()
+
+    def drop_stock(self, ticker, n):
+        self.cursor.execute(f"select * from " + self.type + "portfolio where ticker='{ticker}'")
+        quantity = self.cursor.fetchone()["quantity"]
+        self.cursor.execute(f"update " + self.type + "portfolio set quantity={quantity - n} where ticker='{ticker}'")
+        self.conn.commit()
+
+        if quantity - n <= 0:
+            self.cursor.execute(f"delete from " + self.type + "portfolio where ticker='{ticker}'")
+            self.conn.commit()
+            return
+
+    def insert_trade(self, ticker, type, quantity, price):
+        trade_id = 0
+        with open('last_trade_id.txt', "r+") as f:
+            trade_id = int(f.read())
+            trade_id += 1
+            f.seek(0)
+            f.write(str(trade_id))
+            f.truncate()
+
+        self.cursor.execute(f"insert into " + self.type + "trades (trade_id, ticker, type, quantity, price) values ({trade_id}, "
+                             f"'{ticker}', '{type}', {quantity}, {price})")
+        self.conn.commit()
+
+    def get_stock(self, ticker):
+        self.cursor.execute(f"select * from " + self.type + "portfolio where ticker='{ticker}'")
+        return self.cursor.fetchone()
+
+    def get_trade(self, trade_id):
+        self.cursor.execute(f"select * from " + self.type + "trades where trade_id={trade_id}")
+        return self.cursor.fetchone()
+
+    def get_trades(self, ticker):
+        self.cursor.execute(f"select * from " + self.type + "trades where ticker='{ticker}'")
+        return self.cursor.fetchall()
+
+    def get_valuation(self):
+        self.cursor.execute("select * from " + self.type + "portfolio")
+        stocks = self.cursor.fetchall()
+        return sum(stock["price_per_unit"] for stock in stocks)
+
+    def clean_all(self):
+        if input("Are you sure you want to clean the portfolio and trades tables ?").lower() != "y": return
+
+        self.cursor.execute("delete from " + self.type + "portfolio")
+        self.conn.commit()
+
+        self.cursor.execute("delete from " + self.type + "trades")
+        self.conn.commit()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
--- src/motion_detector/detector.py
+++ src/motion_detector/detector.py
@@ -1,38 +1,138 @@
+# import the necessary packages
+from imutils.video import VideoStream
+import argparse
+import datetime
+import imutils
+import time
 import cv2
 
-# Initialize the camera
-cap = cv2.VideoCapture(0)
+from dbapi import DBAPI
 
-# Create the background subtractor object
-fgbg = cv2.createBackgroundSubtractorMOG2()
+api = DBAPI('hacky_')
 
+stock_i = 0
+tickers = ["AAPL", "TSLA", "AMZN", "GOOGL"]
+
+def get_ticker(stock_i):
+
+	ticker = tickers[stock_i]
+	stock_i += 1
+	if stock_i == len(tickers):
+		stock_i = 0
+	return ticker
+
+def buy(stock_i):
+	api.buy_stock(get_ticker(stock_i), 1, 100)
+	return
+
+def sell(stock_i):
+	#api.sell_stock(get_ticker(), 1, 100)
+	return
+
+buy(stock_i)
+
+"""
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
+# if the video argument is None, then we are reading from webcam
+if args.get("video", None) is None:
+	#vs = cv2.VideoCapture(0)
+	time.sleep(2.0)
+# otherwise, we are reading from a video file
+else:
+	vs = cv2.VideoCapture(args["video"])
+# initialize the first frame in the video stream
+firstFrame = None
+
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+averageX = 0
+fromLeft = True
+canOrder = True
+
+# loop over the frames of the video
 while True:
-    # Read a frame from the camera
-    ret, frame = cap.read()
-    
-    # Apply background subtraction
-    fgmask = fgbg.apply(frame)
-    
-    # Threshold the image
-    th = 2
-    imask = fgmask > th
-    
-    # Count the number of non-zero pixels in the image
-    nz = cv2.countNonZero(imask)
-    
-    # If there are more than 500 non-zero pixels, motion is detected
-    if nz > 500:
-        # Save the current frame as an image
-        cv2.imwrite('motion_detected.jpg', frame)
-        
-    # Display the original and processed frames
-    cv2.imshow('Frame', frame)
-    cv2.imshow('FG Mask', fgmask)
-    
-    # Exit if the user presses 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	# grab the current frame and initialize the occupied/unoccupied
+	# text
+	frame = vs.read()
+	frame = frame if args.get("video", None) is None else frame[1]
+	text = "Unoccupied"
+	# if the frame could not be grabbed, then we have reached the end
+	# of the video
+	if frame is None:
+		break
+	# resize the frame, convert it to grayscale, and blur it
+	frame = imutils.resize(frame, width=500)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+	# if the first frame is None, initialize it
+	if firstFrame is None:
+		firstFrame = gray
+		continue
+	
+  # compute the absolute difference between the current frame and
+	# first frame
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 50, 255, cv2.THRESH_BINARY)[1]
+	# dilate the thresholded image to fill in holes, then find contours
+	# on thresholded image
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	count = 0
+	
+	# loop over the contours
+	for c in cnts:
+		# if the contour is too small, ignore it
+		if cv2.contourArea(c) < args["min_area"]:
+			continue
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+		(x, y, w, h) = cv2.boundingRect(c)
+		count += x
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		text = "Occupied"
+		
+	if len(cnts) != 0 and count != 0:
+		if averageX == 0:
+			fromLeft = count/len(cnts) <= 150
+		averageX = count/len(cnts)
+		
+		if canOrder:
 
-# Release the camera and close all windows
-cap.release()
+			if fromLeft and averageX > 150:
+				buy(stock_i)
+				print("buy")
+				canOrder = False
+			elif not fromLeft and averageX <= 150:
+				sell(stock_i)
+				print("sell")
+				canOrder = False
+			
+		
+	else:
+		canOrder = True
+		averageX = 0
+	
+		
+  	# draw the text and timestamp on the frame
+	cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	
+	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	# show the frame and record if the user presses a key
+	cv2.imshow("Security Feed", frame)
+	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
+	key = cv2.waitKey(1) & 0xFF
+	# if the `q` key is pressed, break from the lop
+	if key == ord("q"):
+		break
+# cleanup the camera and close any open windows
+vs.stop() if args.get("video", None) is None else vs.release()
 cv2.destroyAllWindows()
+"""
